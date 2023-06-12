43695ab971550993dc032a94393df38d7e903104
stathis <stathis@npcglib.org>
2023-06-12 14:17:36 +0300

    Initial SPD RAMP Tool

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# spd-ramp-tool
-SmartPartsDetector tool for RAMP
+# SPDML RAMP Tool
+
+The SmartPartsDetector (SPD) component was designed for assisting the optical detection of small parts in manufacturing facilities, enabling manufacturers to utilize the technology in various tasks on the shop floor (e.g, QA, part sorting, part counting, etc.)
+
+By design the component depends on a trained ML model that is able to recognise the parts of interest, and an external microservice that runs the ML
+model on a given image and stores the predictions on the Orion Context Broker.
+
+We distribute this command line python tool in order to facilitate use of the SPD microservice. 
+
+The latest version of the tool can be downloaded at https://github.com/Algolysis/spd-ramp-tool
+
+
+## Getting started
+
+A user (e.g., factory) registers a model with the microservice, which deploys the model under the hood,
+and then the model can be used to perform object detection tasks. 
+
+```
+python3 spd-ramp-tool.py --token AlgoFactory --registerModel ./some_YOLO_model.pt
+```
+
+The tool has few modes of operation and only demonstrates the basic capability of the SPD technology. Please refer to its usage(```--help```).
+
+You can lest the available models:
+
+```
+python3 spd-ramp-tool.py --token AlgoFactory --list-models
+```
+
+To detect objects in an image you have to use a registered model by passing the ```--model <key>``` parameter:
+
+```
+python3 spd-ramp-tool.py --token AlgoFactory --model my_model_key_ --predict some_input_image.jpg
+```
+
+### LICENSE
+[MIT](LICENSE)
---
+++ spd-ramp-tool.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python3
+
+import argparse
+import os.path
+import requests
+import random
+import string
+import sys
+import pprint
+from urllib.parse import urlencode, quote_plus
+
+######################################################################################################
+
+DEBUG=False
+
+######################################################################################################
+
+def exception_handler(exception_type, exception, traceback, debug_hook=sys.excepthook):
+    if DEBUG:
+        debug_hook(exception_type, exception, traceback)
+    else:
+        print("%s: %s" % (exception_type.__name__, exception))
+
+
+if DEBUG:
+    sys.tracebacklimit=0
+    sys.excepthook = exception_handler
+
+######################################################################################################
+
+def upload_image(url, file_path, token):
+    headers = {'Authorization': f'Bearer {token}'}
+    with open(file_path, 'rb') as file:
+        files = {'images[]': file}
+        try:
+            response = requests.post(url, files=files, headers=headers)
+            return response
+        except requests.exceptions.RequestException as e:
+            print(f"\nError: Failed to connect to the URL '{url}': {str(e)}")
+            return None
+        
+def upload_file(url, file_path, token):
+    headers = {'Authorization': f'Bearer {token}'}
+    with open(file_path, 'rb') as file:
+        files = {'file': file}
+        try:
+            response = requests.post(url, files=files, headers=headers)
+            return response
+        except requests.exceptions.RequestException as e:
+            print(f"\nError: Failed to connect to the URL '{url}': {str(e)}")
+            return None
+      
+
+def register_model(url, model_path, token):
+    # Check if the file exists
+    if not os.path.isfile(model_path):
+        print(f"\nError: Model File \"{model_path}\" not found.")
+        return
+
+    # Upload the file
+    response = upload_file(url, model_path, token)
+
+    if response:
+        print(f"\n{response.json()}\n")
+    else:
+        print(f"\nError: Failed registering \"{model_path}\".")
+
+
+def list_models(url, token):
+    headers = {'Authorization': f'Bearer {token}'}
+    # Upload the file[]
+    try:
+        response = requests.post(url, headers=headers)
+        print('\n')
+        print(response.json())
+        print('\n')
+    except requests.exceptions.RequestException as e:
+        print(f"\nError: Failed to connect to the URL '{url}': {str(e)}")
+        return None
+
+    #if response:
+    #    print(f"\n{response.json()}\n")
+    #else:
+    #    print(f"\nError: Failed registering \"{model_path}\".")
+        
+
+def predict(url, file_path, token, model):
+    # Check if the file exists
+    if not os.path.isfile(file_path):
+        print(f"\nError: Input File \"{file_path}\" not found.")
+        return
+
+    payload = {
+        'model': model, 
+        'wait': "true"
+    }
+    
+    url_with_args = url + "?" + urlencode(payload, quote_via=quote_plus)
+
+    # Upload the file
+    response = upload_image(url_with_args, file_path, token)
+
+    if response:
+        print(f"\n{response.json()}\n")
+    else:
+        print(f"\nError: Failed detecting for \"{file_path}\".")
+
+
+def main():
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description='SmartPartsDetector utility')
+    parser.add_argument('--url', default='https://spd.algolysis.com:5000', help='URL of the SPD microservice.')
+    parser.add_argument('--token', help='Bearer token for authentication.')
+    parser.add_argument('--registerModel', help='Path to the model file to register in the SPD microservice.')
+    parser.add_argument('--list-models', action='store_true', help='List available models for the specified token.')
+    parser.add_argument('--predict', help='Object detection mode.')
+    parser.add_argument('--model', help='Model to use for object detection')
+    args = parser.parse_args()
+
+    print("\nExecuting SPDML Tool in: " + os.getcwd())
+    if args.url:
+        print("   SPD Microservice URL: " + args.url)
+    else:
+        print("\nERROR: Please specify a valid SPD URL.")
+        sys.exit()
+    
+    if args.token:
+        print("           Bearer Token: " + args.token)
+    else:
+        print("\nERROR: Please specify a token.")
+        sys.exit()
+
+    if args.registerModel:
+        print("             Model Path: " + args.registerModel)
+        register_model(args.url + "/registerModel", args.registerModel, args.token)
+    elif args.list_models:
+        print("         Listing models: ")
+        list_models(args.url + "/listModels", args.token)   
+    elif args.predict:
+        if args.model:
+            print("   Detecting Objects in: " + args.predict)
+            predict(args.url + "/detect", args.predict, args.token, args.model)
+        else:
+            print("\nERROR: Please specify a model for object detection.\nUse --list-models to see available models.")
+            sys.exit()
+    else:
+        parser.print_help()
+
+
+if __name__ == '__main__':
+    main()
+
